96775035a000c6cbfa12a96db7b9dec969a30e68
dzakyputra45 <dzakypratama45@gmail.com>
2025-10-29 09:04:11 +0700

    Tambahkan Implementasi delete dan update

--- 20230140245-node-server/controllers/presensiController.js
+++ 20230140245-node-server/controllers/presensiController.js
@@ -88,3 +88,50 @@ exports.CheckOut = async (req, res) => {
   }
 };
 
+exports.deletePresensi = async (req, res) => {
+  try {
+    const { id: userId } = req.user;
+    const presensiId = req.params.id;
+    const recordToDelete = await Presensi.findByPk(presensiId);
+
+    if (!recordToDelete) {
+      return res
+        .status(404)
+        .json({ message: "Catatan presensi tidak ditemukan." });
+    }
+    if (recordToDelete.userId !== userId) {
+      return res
+        .status(403)
+        .json({ message: "Akses ditolak: Anda bukan pemilik catatan ini." });
+    }
+    await recordToDelete.destroy();
+    res.status(204).send();
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Terjadi kesalahan pada server", error: error.message });
+  }
+};
+
+
+
+exports.updatePresensi = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { nama } = req.body;
+
+    const data = await Presensi.findByPk(id);
+    if (!data) {
+      return res.status(404).json({ message: "Catatan presensi tidak ditemukan." });
+    }
+
+    data.nama = nama || data.nama;
+    await data.save();
+
+    res.json({ message: "Data presensi berhasil diupdate", data });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Terjadi kesalahan pada server" });
+  }
+};
+
