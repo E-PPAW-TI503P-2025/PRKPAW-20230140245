df718c0dcc2ccf5a0f898292a034b4808b37de38
dzakyputra45 <dzakypratama45@gmail.com>
2025-12-24 10:11:19 +0700

    Tambahkan IOT

--- 20230140245-node-server/server.js
+++ 20230140245-node-server/server.js
@@ -11,6 +11,7 @@ const path = require("path");
 const presensiRoutes = require("./route/presensi");
 const reportRoutes = require("./route/report");
 const authRoutes = require("./route/auth");
+const iotRoutes = require("./route/iot");
 
 app.use("/upload", express.static(path.join(__dirname, "upload")));
 
@@ -32,6 +33,7 @@ app.use("/api/books", ruteBuku);
 app.use("/api/presensi", presensiRoutes);
 app.use("/api/reports", reportRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/iot", iotRoutes);
 app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
 });
